9ce81bb8235687c6bb70985f0ced88d2a6e01198
mark sigilai <sigilaimark@gmail.com>
2023-12-28 15:06:10 -0800

    moving files out of the app file:

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image';
+import styles from './styles.module.css';
+
+export default function Home() {
+	return (
+		<main className={styles.main}>
+			<section>Home page</section>
+		</main>
+	);
+}
---
+++ src/app/team/page.tsx
@@ -0,0 +1,6 @@
+import Image from 'next/image';
+import styles from './styles.module.css';
+
+export default function Team() {
+	return <div className={styles.main}>team page</div>;
+}
