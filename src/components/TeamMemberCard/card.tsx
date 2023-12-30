97b9d7e8b22fd04011781720896e5627e906e5b9
MatthewOlaka <matthew.olaka0@gmail.com>
2023-12-30 13:09:08 -0800

    First iteration of the teams page with provided data

---
+++ src/components/TeamMemberCard/card.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import styles from './styles.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faFacebook, faTwitter, faLinkedin } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook, faXTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
+
+
+interface IProps {
+    name: string;
+    position: string;
+    description: string;
+    image: string;
+    socialMedia?: {
+        facebook?: string;
+        twitter?: string;
+        linkedin?: string;
+        // Add more social media as needed
+    };
+}
+
+const TeamMemberCard = (props: IProps) => {
+    const { name, description, image, position, socialMedia } = props;
+    return (
+        <div className={ styles.card }>
+            <img src={ image } alt={`${name}'s headshot`} className={ styles.image } />
+            <h3 className={ styles.name }>{ name }</h3>
+            <span className={styles.line} />
+            <p className={ styles.position }>{ position }</p>
+            <p className={ styles.description }>{ description }</p>
+            { socialMedia && (
+                <div className={styles.socialMedia}>
+                    {socialMedia.facebook && (
+                        <a href={socialMedia.facebook} target="_blank" rel="noopener noreferrer">
+                            <FontAwesomeIcon icon={ faFacebook } style={{ "color": "var(--facebookBlue)" }}/>
+                        </a>
+                    )}
+                    {socialMedia.twitter && (
+                        <a href={socialMedia.twitter} target="_blank" rel="noopener noreferrer">
+                            <FontAwesomeIcon icon={ faXTwitter } className={ styles.icon }/>
+                        </a>
+                    )}
+                    {socialMedia.linkedin && (
+                        <a href={socialMedia.linkedin} target="_blank" rel="noopener noreferrer">
+                            <FontAwesomeIcon icon={ faLinkedin } className={ styles.icon }/>
+                        </a>
+                    )}
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default TeamMemberCard
