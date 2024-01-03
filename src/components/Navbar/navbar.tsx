8e1f8345d3e4f8e7dfe3dfeac83230732617b7c1
Dennis Arimi <mwirigiarimi@gmail.com>
2024-01-02 20:08:35 -0800

    navbar

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -11,25 +11,27 @@ const Navbar = (props: IProps) => {
 	const { currentPath = '/' } = props;
 
 	return (
-		<nav className={styles.navbar}>
-			<div className={styles.links}>
-				<Link href={'/'} className={`${styles.link} ${currentPath === '/' ? styles.link_active : ''}`}>
-					HOME
-				</Link>
-				<Link href={'/results'} className={`${styles.link} ${currentPath === '/resuts' ? styles.link_active : ''}`}>
-					RESULTS
-				</Link>
-				<span className={styles.logo}>
-					<Image src={'/logo.png'} alt='logo' width={40} height={45} />
-				</span>
-				<Link href={'/resources'} className={`${styles.link} ${currentPath === '/resources' ? styles.link_active : ''}`}>
-					RESOURCES
-				</Link>
-				<Link href={'/team'} className={`${styles.link} ${currentPath === '/team' ? styles.link_active : ''}`}>
-					MEET THE TEAM
-				</Link>
-			</div>
-		</nav>
+		<div className={styles.nav_box}>
+			<nav className={styles.navbar}>
+				<div className={styles.links}>
+					<Link href={'/'} className={`${styles.link} ${currentPath === '/' ? styles.link_active : ''}`}>
+						HOME
+					</Link>
+					<Link href={'/results'} className={`${styles.link} ${currentPath === '/resuts' ? styles.link_active : ''}`}>
+						RESULTS
+					</Link>
+					<Link href={'/'} className={`${styles.link} ${currentPath === '/' ? styles.link_active : ''}`}>
+						<Image src={'/logos/winrack_tiny_logo.svg'} alt='logo' width={75} height={76} />
+					</Link>
+					<Link href={'/resources'} className={`${styles.link} ${currentPath === '/resources' ? styles.link_active : ''}`}>
+						RESOURCES
+					</Link>
+					<Link href={'/team'} className={`${styles.link} ${currentPath === '/team' ? styles.link_active : ''}`}>
+						MEET THE TEAM
+					</Link>
+				</div>
+			</nav>
+		</div>
 	);
 };
 
