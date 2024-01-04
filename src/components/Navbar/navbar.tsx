1871af00870383cd97c00473a11aa9ea2ef9d72f
Dennis Arimi <mwirigiarimi@gmail.com>
2024-01-03 18:32:36 -0800

    underline effect alterations

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -1,7 +1,9 @@
+"use client";
 import { Fragment, ReactNode, useState, useEffect } from 'react';
 import styles from './styles.module.css';
 import Link from 'next/link';
 import Image from 'next/image';
+import { usePathname } from "next/navigation";
 
 interface IProps {
 	currentPath?: string;
@@ -9,24 +11,25 @@ interface IProps {
 
 const Navbar = (props: IProps) => {
 	const { currentPath = '/' } = props;
+	const pathname = usePathname();
 
 	return (
 		<div className={styles.nav_box}>
 			<nav className={styles.navbar}>
 				<div className={styles.links}>
-					<Link href={'/'} className={`${styles.link} ${currentPath === '/' ? styles.link_active : ''}`}>
+					<Link href={'/'} className={pathname == "/" ? styles.link_active : styles.link}>
 						HOME
 					</Link>
-					<Link href={'/results'} className={`${styles.link} ${currentPath === '/resuts' ? styles.link_active : ''}`}>
+					<Link href={'/results'} className={pathname == "/results" ? styles.link_active : styles.link}>
 						RESULTS
 					</Link>
-					<Link href={'/'} className={`${styles.link} ${currentPath === '/' ? styles.link_active : ''}`}>
+					<Link href={'/'}>
 						<Image src={'/logos/winrack_tiny_logo.svg'} alt='logo' width={75} height={76} />
 					</Link>
-					<Link href={'/resources'} className={`${styles.link} ${currentPath === '/resources' ? styles.link_active : ''}`}>
+					<Link href={'/resources'} className={pathname == "/resources" ? styles.link_active : styles.link}>
 						RESOURCES
 					</Link>
-					<Link href={'/team'} className={`${styles.link} ${currentPath === '/team' ? styles.link_active : ''}`}>
+					<Link href={'/team'} className={pathname == "/team" ? styles.link_active : styles.link}>
 						MEET THE TEAM
 					</Link>
 				</div>
