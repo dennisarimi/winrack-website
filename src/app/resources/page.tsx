c5c4743304ce1270d3b4cb4d41803afb55227c80
Dennis Arimi <mwirigiarimi@gmail.com>
2024-01-05 17:22:26 -0800

    creating fileLinkCard and initial page setup

--- src/app/resources/page.tsx
+++
@@ -1,6 +0,0 @@
-import Image from 'next/image';
-import styles from './styles.module.css';
-
-export default function Home() {
-	return <div className={styles.main}>resources page</div>;
-}
--- src/app/results/page.tsx
+++
@@ -1,6 +0,0 @@
-import Image from 'next/image';
-import styles from './styles.module.css';
-
-export default function Results() {
-	return <div className={styles.main}>results page</div>;
-}
